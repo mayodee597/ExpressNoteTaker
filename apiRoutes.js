1da4483423eecd01950825b3778ad32f63b25b9d
Mayo Dada <mayodee13@gmail.com>
2020-08-11 21:32:50 -0400

    Add files via upload

---
+++ apiRoutes.js
@@ -0,0 +1,53 @@
+var path = require("path");
+var fs = require("fs");
+var savedNotesFile = require("../db/db.json");
+
+
+module.exports = function (app) {
+  app.get("/api/notes", (req, res) => {
+    
+    fs.readFile("app/db/db.json", "utf8", (err,data) =>{
+      if (err) throw err;
+      const allNotes = JSON.parse(data);
+      res.json(allNotes);
+    })
+    
+  });
+
+  app.post("/api/notes", (req, res) => {
+
+// get info from new note -- in the req.body
+    console.log("body is", req.body);
+    let noteTitle = req.body.noteTitle;
+    let noteContent = req.body.noteContent;
+    let noteInfo = { title: noteTitle, text: noteContent };
+    fs.readFile("app/db/db.json", "utf8", (err,data) =>{
+      if (err) throw err;
+      const allNotes = JSON.parse(data);
+      allNotes.push(noteInfo);
+      fs.writeFile("app/db/db.json", JSON.stringify(allNotes, null, 2), err =>{
+        if (err) throw err;
+        res.send(allNotes);
+        console.log("note created")
+        console.log(noteTitle, noteContent);
+      })
+  });
+  })
+
+  app.delete("/api/notes/:id",  (req, res) => {
+    const noteId = req.params.id;
+    fs.readFile("app/db/db.json", "utf8", (err,data) =>{
+      if (err) throw err;
+      const allNotes = JSON.parse(data);
+      const newNotes = allNotes.filter(note => note.id != noteId );
+      fs.writeFile("app/db/db.json", JSON.stringify(newNotes, null, 2), err =>{
+        if (err) throw err;
+        res.send(savedNotesFile);
+        console.log("note deleted")
+        })
+    })
+  })
+};
+
+
+
